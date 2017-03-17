641c5eabb2bf36c8e9be3b7d752b53eaaa2578f3
Quang Pham <ptquang86@gmail.com>
2017-03-17 16:53:27 +0700

    fix local image

--- imageview.js
+++ imageview.js
@@ -30,14 +30,14 @@ exports.createImageView = function(args) {
 				_view.resizeImage();
 			} else {
 				var imageview = Ti.UI.createImageView({ opacity: 0.01, width: Ti.UI.SIZE, height: Ti.UI.SIZE, touchEnabled: false });
-					imageview.addEventListener('load', _view.resizeImage);
+					imageview.addEventListener('load', _resizeImage);
 					imageview.addEventListener('error', function(e) {
 						var _imageview = this;
 						_imageview.image = _imageview.parent.brokenLinkImage;
 						_imageview.animate({ opacity: 1, duration: 300 });
 					});
-					_view.image && (imageview.image = _view.image);
 					_view.add(imageview);
+					_view.image && view.setImage(_view.image);
 			}
 		});
 		
@@ -47,38 +47,58 @@ exports.createImageView = function(args) {
 function _setImage(image) {
 	this.image = image;
 	var _imageview = this.children[0];
-	_imageview && (_imageview.image = image);
+	if (_imageview) {
+		_imageview.removeEventListener('load', _resizeImage); 
+		_imageview.addEventListener('load', _resizeImage);
+		_imageview.image = image;
+	}
 }
 
 function _resizeImage(e) {
 	var _imageview = this;
-	if (e == null) { _imageview = _imageview.children[0]; }
+	if (e) { 
+		_imageview.removeEventListener('load', _resizeImage); 
+	} else {
+		_imageview = _imageview.children[0];
+	}
 	
 	var _view = _imageview.parent,
 		viewWidth = _view.rect.width,
-		viewHeight = _view.rect.height,
-		imageviewWidth  = _imageview.rect.width,
-		imageviewHeight = _imageview.rect.height,
-		ratio = imageviewWidth / imageviewHeight;
+		viewHeight = _view.rect.height;
+	
+	var _imageblob = _imageview.toBlob(),
+		imageviewWidth  = _imageblob.width,
+		imageviewHeight = _imageblob.height;
+		
+	if (imageviewWidth == viewWidth && imageviewHeight == viewHeight) { 
+		return _removeDefaultImage(_view); 
+	}
+	
+	var ldf = Ti.Platform.displayCaps.logicalDensityFactor;
+	if (OS_ANDROID) { ldf = 1; }
+	
+	if (imageviewWidth != viewWidth && imageviewHeight != viewHeight) {
+		var size = (viewWidth > viewHeight ? viewWidth : viewHeight);
+		_imageblob = _imageblob.imageAsThumbnail(size * ldf, 0, 0);
+	}
+	
+	_imageview.image = _imageblob.imageAsCropped({
+		width: viewWidth * ldf, 
+		height: viewHeight * ldf, 
+		x: Math.floor((_imageblob.width  - viewWidth) / 2),
+		y: Math.floor((_imageblob.height - viewHeight) / 2)
+	});
+	
+	_imageview.width = viewWidth;
+	_imageview.height = viewHeight;
+	
+	_removeDefaultImage(_view);
+	
+	_imageview.animate({ opacity: 1, duration: 300 });
+}
 
+function _removeDefaultImage(_view) {
 	if (_view.backgroundImage == _view.defaultImage) {
 		_view.backgroundImage = '';
 	}
-
-	if (imageviewWidth == viewWidth && imageviewHeight == viewHeight) { return; }
-		
-	if (imageviewHeight < viewHeight) {
-		imageviewHeight = viewHeight;
-		imageviewWidth  = ratio * imageviewHeight;
-	}
-	
-	if (imageviewWidth < viewWidth) {
-		imageviewWidth = viewWidth;
-		imageviewHeight = imageviewWidth / ratio;
-	}
-	
-	imageviewWidth = Math.floor(imageviewWidth);
-	imageviewHeight = Math.floor(imageviewHeight);
-	
-	_imageview.animate({ opacity: 1, duration: 300 });
 }
